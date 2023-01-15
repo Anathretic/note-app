ced2b60411eb131b94dc724558f42cb62d8641ea
Konrad <konrad.wojtylo.9@gmail.com>
2023-01-15 19:30:43 +0100

    optimize js

--- js/main.js
+++ js/main.js
@@ -1,69 +1,95 @@
-const navAddBtn = document.querySelector('.nav__menu-buttons-add')
-const navDeleteAllBtn = document.querySelector('.nav__menu-buttons-deleteall')
-const noteArea = document.querySelector('.note-area')
-const popup = document.querySelector('.popup-shadow')
-const popupCategoryInput = document.querySelector('#category')
-const popupTextInput = document.querySelector('#text')
-const popupErrorMsg = document.querySelector('.popup__error')
-const popupSaveBtn = document.querySelector('.popup__buttons-save')
-const popupCancelBtn = document.querySelector('.popup__buttons-close')
-const noteDeleteBtn = document.getElementsByClassName('notecard__icon')
-const returnArrow = document.querySelector('.return__arrow')
+let NAV_ADD_BTN
+let NAV_DELETE_ALL_BTN
+let NOTE_AREA
+let POPUP
+let POPUP_CATEGORY_INPUT
+let POPUP_TEXT_INPUT
+let POPUP_ERROR_MSG
+let POPUP_SAVE_BTN
+let POPUP_CANCEL_BTN
+let NOTE_DELETE_BTN
+let RETURN_ARROW
+let CARD_ID
+let SELECTED_CATEGORY
 
-const textLength = 400
+const main = () => {
+	prepareDOMElements()
+	prepareDOMEvents()
+}
 
-let cardID = 0
-let selectedCategory
+const prepareDOMElements = () => {
+	NAV_ADD_BTN = document.querySelector('.nav__menu-buttons-add')
+	NAV_DELETE_ALL_BTN = document.querySelector('.nav__menu-buttons-deleteall')
+	NOTE_AREA = document.querySelector('.note-area')
+	POPUP = document.querySelector('.popup-shadow')
+	POPUP_CATEGORY_INPUT = document.querySelector('#category')
+	POPUP_TEXT_INPUT = document.querySelector('#text')
+	POPUP_ERROR_MSG = document.querySelector('.popup__error')
+	POPUP_SAVE_BTN = document.querySelector('.popup__buttons-save')
+	POPUP_CANCEL_BTN = document.querySelector('.popup__buttons-close')
+	NOTE_DELETE_BTN = document.getElementsByClassName('notecard__icon')
+	RETURN_ARROW = document.querySelector('.return__arrow')
+	CARD_ID = 0
+	SELECTED_CATEGORY
+}
+
+const prepareDOMEvents = () => {
+	window.addEventListener('scroll', showArrow)
+	NAV_ADD_BTN.addEventListener('click', showPopup)
+	NAV_DELETE_ALL_BTN.addEventListener('click', deleteAllNotes)
+	POPUP_SAVE_BTN.addEventListener('click', checkInputs)
+	POPUP_CANCEL_BTN.addEventListener('click', closePopup)
+}
 
 const showPopup = () => {
-	popup.style.display = 'flex'
+	POPUP.style.display = 'flex'
 }
 
 const closePopup = () => {
-	popup.style.display = 'none'
+	POPUP.style.display = 'none'
 	clearPopup()
 }
 
 const checkInputs = () => {
-	if (popupTextInput.value !== '' && popupCategoryInput.options[popupCategoryInput.selectedIndex].value !== '0') {
+	if (POPUP_TEXT_INPUT.value !== '' && POPUP_CATEGORY_INPUT.options[POPUP_CATEGORY_INPUT.selectedIndex].value !== '0') {
 		createNote()
 	} else {
-		popupErrorMsg.style.visibility = 'visible'
+		POPUP_ERROR_MSG.style.visibility = 'visible'
 	}
 }
 
 const clearPopup = () => {
-	popupCategoryInput.selectedIndex = 0
-	popupTextInput.value = ''
-	popupErrorMsg.style.visibility = 'hidden'
+	POPUP_CATEGORY_INPUT.selectedIndex = 0
+	POPUP_TEXT_INPUT.value = ''
+	POPUP_ERROR_MSG.style.visibility = 'hidden'
 }
 
 const createNote = () => {
 	const newNote = document.createElement('div')
 	newNote.classList.add('notecard')
-	newNote.setAttribute('id', cardID)
+	newNote.setAttribute('id', CARD_ID)
 	newNote.innerHTML = `
                     <div class="notecard__header">
-                        <h3 class="notecard__title">${selectedCategory}</h3>
-                        <button class="notecard__icon" onclick="deleteNote(${cardID})">
+                        <h3 class="notecard__title">${SELECTED_CATEGORY}</h3>
+                        <button class="notecard__icon" onclick="deleteNote(${CARD_ID})">
                             <i class="fas fa-times icon"></i>
                         </button>
                     </div>
                     <div class="notecard__body">
-                        ${popupTextInput.value}
+                        ${POPUP_TEXT_INPUT.value}
                     </div>`
-	noteArea.appendChild(newNote)
+	NOTE_AREA.appendChild(newNote)
 	checkCategory(newNote)
-	cardID++
+	CARD_ID++
 	closePopup()
 }
 
 const selectCategory = () => {
-	selectedCategory = popupCategoryInput.options[popupCategoryInput.selectedIndex].text
+	SELECTED_CATEGORY = POPUP_CATEGORY_INPUT.options[POPUP_CATEGORY_INPUT.selectedIndex].text
 }
 
 const checkCategory = note => {
-	switch (selectedCategory) {
+	switch (SELECTED_CATEGORY) {
 		case 'Work':
 			note.style.backgroundColor = '#df2b20'
 			break
@@ -84,25 +110,21 @@ const checkCategory = note => {
 
 const deleteNote = id => {
 	const noteToDelete = document.getElementById(id)
-	noteArea.removeChild(noteToDelete)
+	NOTE_AREA.removeChild(noteToDelete)
 }
 
 const deleteAllNotes = () => {
-	noteArea.textContent = ''
+	NOTE_AREA.textContent = ''
 }
 
 const showArrow = () => {
-    if(window.scrollY > 100) {
-        returnArrow.style.display = 'block'
-    } else {
-        returnArrow.style.display = 'none'
-    }
+	if (window.scrollY > 100) {
+		RETURN_ARROW.style.display = 'block'
+	} else {
+		RETURN_ARROW.style.display = 'none'
+	}
 
-    returnArrow.classList.toggle('arrow-up-animation')
+	RETURN_ARROW.classList.toggle('arrow-up-animation')
 }
 
-window.addEventListener('scroll', showArrow)
-navAddBtn.addEventListener('click', showPopup)
-navDeleteAllBtn.addEventListener('click', deleteAllNotes)
-popupSaveBtn.addEventListener('click', checkInputs)
-popupCancelBtn.addEventListener('click', closePopup)
+document.addEventListener('DOMContentLoaded', main)
